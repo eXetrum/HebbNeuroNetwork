8a753b1f14480b64d145f6dacc92dc2c25f99285
eXetrum <eXetrum@gmail.com>
2015-10-30 17:34:35 +0200

    "Comments"

--- HebbNeuralNetwork/mainForm.cs
+++ HebbNeuralNetwork/mainForm.cs
@@ -15,10 +15,11 @@ namespace HebbNeuralNetwork
         static int rows = 5;
         static int cols = 4;
         static int standartInputCount = 4;
+        // Интерфейс ввода символов
         InputGUI[] inputGUI = new InputGUI[standartInputCount + 1];
-
+        // Символы
         string[] CHARS = { "P", "O", "K", "E" };
-
+        // Биполярное представление букв
         List<int[]> M = new List<int[]>() {
             // Буква "Р"
             new int[] {
@@ -53,39 +54,43 @@ namespace HebbNeuralNetwork
                 1, 1, 1, 1
             }
         };
+        // Создаем нейронную сеть
         NeuroNetwork nn = new NeuroNetwork();
+
         public mainForm()
         {
             InitializeComponent();
-
+            // Создаем интерфейс ввода
             for (int i = 0; i < standartInputCount; ++i)
             {
+                // Интерфейс сетка rows X cols
                 inputGUI[i] = new InputGUI(rows, cols);
+                // Добавляем новый gui в коллекцию элементов панели
                 inputGUI[i].attachTo(standartInputPanel);
+                // Задаем изображение
                 inputGUI[i].SetImage(M[i]);
             }
-
+            // Последний gui оставим для ввода пользовательского изображения
             inputGUI[inputGUI.Length - 1] = new InputGUI(rows, cols);
+            // Прикрепляем gui
             inputGUI[inputGUI.Length - 1].attachTo(userInputPanel, true);
-
+            // Изображение пользователя
             int[] userX = new int[rows * cols];
+            // 
             Random rnd = new Random(DateTime.Now.Millisecond);
+            // Заполним вектор изображения случ. данными
             for (int i = 0; i < rows * cols; ++i) userX[i] = (rnd.Next(0, 2) == 0 ? 1 : -1);
-
+            // Заносим изображение
             inputGUI[inputGUI.Length - 1].SetImage(userX);
-
-
         }
 
         private void btnTrain_Click(object sender, EventArgs e)
         {
-            btnRecognize.Enabled = true;
+            // Задаем логгер для вывода промежуточных шагов состояний сети
             NeuroNetwork.log = log;
-            
-            List<NeuroNetwork.PairSet> m = new List<NeuroNetwork.PairSet>();
-
-            
-            
+            // Множество пар: изображение - ожидаемый выходной вектор сети (Тренировочная выборка)
+            List<NeuroNetwork.PairSet> m = new List<NeuroNetwork.PairSet>();           
+            // Считываем данные о всех изображениях
             for(int i = 0; i < standartInputCount; ++i) 
             {
                 m.Add(new NeuroNetwork.PairSet()
@@ -93,46 +98,46 @@ namespace HebbNeuralNetwork
                     image = inputGUI[i].GetImage(),
                     t = new int[standartInputCount]
                 });
-
+                // Ожидаемый выходной вектор сети для текущего изображения
                 for (int ti = 0; ti < standartInputCount; ++ti)
                     m[i].t[ti] = -1;
                 m[i].t[i] = 1;
             }
+            // Обучаем сеть
             nn.Train(rows * cols, standartInputCount, m);
-
-            
-            
-
+            // Включаем возможность распознования только после обучения сети
+            btnRecognize.Enabled = true;
         }
 
         private void btnRecognize_Click(object sender, EventArgs e)
         {
+            // Получаем пользовательское изображение для распознавания
             int[] userImage = inputGUI[inputGUI.Length - 1].GetImage();
-            int[] o = nn.Recognize(userImage);
-
-            
+            // Отдаем изображение для распознавания - получаем выходной вектор
+            int[] userImageResult = nn.Recognize(userImage);
+            // Находим максимум среди выходных значений сети
             int maxid = 0;
-            int max = o[0];
-            for (int i = 1; i < o.Length; ++i)
-                if (o[maxid] < o[i]) 
+            int max = userImageResult[0];
+            for (int i = 1; i < userImageResult.Length; ++i)
+                if (userImageResult[maxid] < userImageResult[i]) 
                 {
                     maxid = i;
-                    max = o[i];
+                    max = userImageResult[i];
                 }
-
             
             List<string> result = new List<string>();
-            for (int i = 0; i < o.Length; ++i)
-                if (o[i] == max) result.Add(CHARS[i]);
-
+            for (int i = 0; i < userImageResult.Length; ++i)
+                // Добавляем все вероятные символы (если таковых множество)
+                if (userImageResult[i] == max) result.Add(CHARS[i]);
+            // Формируем результат - строку
             string resultSTR = "";
             for (int i = 0; i < result.Count; ++i)
                 resultSTR += result[i] + (i + 1 < result.Count ? ", " : "");
-
-            for (int i = 0; i < o.Length; ++i)
-                log.AppendText(o[i] + " ");
+            // Отладочная информация
+            for (int i = 0; i < userImageResult.Length; ++i)
+                log.AppendText(userImageResult[i] + " ");
             log.AppendText(Environment.NewLine);
-
+            // Вывод результата
             log.AppendText("Вероятный символ: " + resultSTR + Environment.NewLine);
         }
 
